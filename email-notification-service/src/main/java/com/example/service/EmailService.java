025c2bf28a66ea5ceefeb987763113a1172fcf51
srikanthgenji <104547423+srikanthgenji@users.noreply.github.com>
2022-05-15 09:21:47 +0530

    Delete EmailService.java

--- email-notification-service/src/main/java/com/example/service/EmailService.java
+++
@@ -1,9 +0,0 @@
-package com.example.service;
-
-import com.example.entity.EmailDetails;
-
-public interface EmailService {
-	// Method
-    // To send a simple email
-    String sendSimpleMail(EmailDetails details);
-}
